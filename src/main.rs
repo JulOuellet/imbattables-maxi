3cf2289fd5d8ba0b34dabfe1d95eb21039e19b5f
JulOuellet <julien.ouellet@live.ca>
2024-08-31 18:52:37 -0400

    add libraries

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::Client;
+use scraper::{Html, Selector};
+use tokio;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client = Client::new();
+    let metro_url = "https://www.metro.ca/epicerie-en-ligne/circulaire";
+    let res = client.get(metro_url).send().await?;
+
+    if !res.status().is_success() {
+        panic!("Failed to get the Metro website");
+    };
+
+    let document = Html::parse_document(&res.text().await?);
+
+    Ok(())
 }
+
