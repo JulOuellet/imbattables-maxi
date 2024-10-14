a4ad0834824b06f432dd968c8eac90d782b9c443
JulOuellet <julien.ouellet@live.ca>
2024-10-14 19:43:52 -0400

    refactor with new trait

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,13 @@
-use reqwest::Client;
-use scraper::{Html, Selector};
+mod scraper;
+
+use scraper::{MetroScraper, Scraper};
 use tokio;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let client = Client::new();
-    let metro_url = "https://www.metro.ca/epicerie-en-ligne/circulaire";
-    let res = client.get(metro_url).send().await?;
 
-    if !res.status().is_success() {
-        panic!("Failed to get the Metro website");
-    };
-
-    let document = Html::parse_document(&res.text().await?);
+    let metro_scraper = MetroScraper::new();
+    metro_scraper.scrape_products().await?;
 
     Ok(())
 }
